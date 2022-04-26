c8ab3ef95786ffa83a68146f732093ab17287419
mtyb <50377736+mtyb@users.noreply.github.com>
2022-04-26 22:43:21 +0200

    Update vsts_agent_onboard.sh

--- vsts_agent_onboard.sh
+++ vsts_agent_onboard.sh
@@ -1,12 +1,12 @@
 cd /
 #create installation directory
 sudo mkdir myagent
+sudo chmod -R o+w myagent
 cd myagent
 #Download and install the agent
 sudo wget "https://vstsagentpackage.azureedge.net/agent/2.202.1/vsts-agent-linux-x64-2.202.1.tar.gz"
 sudo tar zxvf *.tar.gz
-sudo chmod -R o+w /myagent
 #Install dependencies
-sudo /myagent/bin/installdependencies.sh
+/myagent/bin/installdependencies.sh
 #Configure agent
 /myagent/config.sh --unattended --token $1 --url $2 --auth $3 --pool $4 --runAsService --acceptTeeEula
