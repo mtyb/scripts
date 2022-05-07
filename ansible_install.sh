0cff0c4ccb1d977f8a8af149df0592905986c35b
mtyb <50377736+mtyb@users.noreply.github.com>
2022-05-07 15:46:34 +0200

    Update ansible_install.sh

--- ansible_install.sh
+++ ansible_install.sh
@@ -1,4 +1,5 @@
-yum update && yum upgrade -y
+yum update -y
+yum upgrade -y
 yum install git -y
 yum install python3 python3-pip python-setuptools python-yaml gcc python-dev libkrb5-dev sshpass -y
 pip3 install --upgrade pip
