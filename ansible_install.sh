4e533d73183d68853b136afbbd0082fe569dd769
mtyb <50377736+mtyb@users.noreply.github.com>
2022-05-07 19:00:38 +0200

    Update ansible_install.sh

--- ansible_install.sh
+++ ansible_install.sh
@@ -1,5 +1,13 @@
 yum update -y
 yum upgrade -y
+cat > /etc/yum.repos.d/WANdisco-git.repo << EOF
+[WANdisco-git]
+name=WANdisco Git
+baseurl=http://opensource.wandisco.com/rhel/\$releasever/git/\$basearch
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-WANdisco
+EOF
+rpm --import http://opensource.wandisco.com/RPM-GPG-KEY-WANdisco
 yum install git -y
 yum install python3 python3-pip python-setuptools python-yaml gcc python-dev libkrb5-dev sshpass -y
 pip3 install --upgrade pip
