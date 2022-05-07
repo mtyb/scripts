4ccdfc99c84c23d7b58649c64161452184c90543
mtyb <50377736+mtyb@users.noreply.github.com>
2022-05-07 13:03:24 +0200

    Update ansible_install.sh

--- ansible_install.sh
+++ ansible_install.sh
@@ -1,3 +1,3 @@
-yum install python3 python3-pip python-setuptools python-yaml gcc python-dev libkrb5-dev sshpass -y
+yum install git python3 python3-pip python-setuptools python-yaml gcc python-dev libkrb5-dev sshpass -y
 pip3 install --upgrade pip
 pip3 install ansible[azure] pywinrm[credssp]
