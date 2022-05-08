548e3fdce76c4710d83ac263613ae1e42f089eab
mtyb <50377736+mtyb@users.noreply.github.com>
2022-05-08 20:14:49 +0200

    Delete ansible_install_ubuntu.sh

--- ansible_install_ubuntu.sh
+++
@@ -1,6 +0,0 @@
-apt-get update -y
-apt-get upgrade -y
-apt-get install git -y
-apt-get install python3 python3-pip python-setuptools python-yaml gcc python-dev libkrb5-dev sshpass -y
-pip3 install --upgrade pip
-pip3 install ansible[azure] pywinrm[credssp]
