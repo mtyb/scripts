be86f090f3eb899293a7a1153d655b1d42287be9
MTG <maciej.tyborowski@gmail.com>
2023-02-21 17:22:22 +0000

    script update

--- ansible_install_new.sh
+++ ansible_install_new.sh
@@ -1,4 +1,8 @@
+# install EPEL repo
 dnf install epel-release -y
+# install ansible and other dependencies
 dnf install ansible openssl python39-winrm.noarch python39-jmespath -y
+# install CredSSP package
 pip3 install requests-credssp
+# install community.windows collection
 ansible-galaxy collection install community.windows:1.9.0 -p /usr/lib/python3.9/site-packages/ansible_collections 
